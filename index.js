ee21d3fb5f41a39439a41e55347134c343e20769
Mahbubullah <mahbubullahpathan@gmail.com>
2023-08-19 00:13:35 +0600

    tiangle js

---
+++ index.js
@@ -0,0 +1,47 @@
+
+function getInputFieldValue(fieldId){
+    const inputField = document.getElementById(fieldId)
+    const inputFieldString = inputField.value;
+       inputField.value='';
+    const inputFieldFloat= parseFloat(inputFieldString);
+
+ 
+
+    return inputFieldFloat;
+  
+}
+
+
+function setText(id,area){
+
+    const textId= document.getElementById(id);
+        textId.innerHTML=area;
+
+
+}
+
+
+
+
+
+
+
+function triangleArea() {
+    const base= getInputFieldValue('triangleBase');
+    
+    const height= getInputFieldValue('triangleHeight');
+    if(isNaN(base) || isNaN(height)){
+        alert("Provide a valid number");
+       return;
+    }
+
+   const area= 0.5*base*height;
+   console.log(area);
+   setText('triangleArea', area);
+   
+
+
+    
+
+
+}
